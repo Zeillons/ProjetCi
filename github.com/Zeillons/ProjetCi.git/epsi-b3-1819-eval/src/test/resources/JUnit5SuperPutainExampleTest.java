1d52cf2962a590371d357274ddf82dd41bac3620
Zeillons <tgc@live.fr>
2019-03-20 11:40:41 +0100

    rerererereretest

---
+++ github.com/Zeillons/ProjetCi.git/epsi-b3-1819-eval/src/test/resources/JUnit5SuperPutainExampleTest.java
@@ -0,0 +1,15 @@
+package test.resources;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUnit5SuperPutainExampleTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented sheyuhhh");
+	}
+
+}
