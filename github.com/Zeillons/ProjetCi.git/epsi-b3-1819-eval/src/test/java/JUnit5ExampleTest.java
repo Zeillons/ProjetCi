4cbd8c082b2ae8008f563288d5cc59d30c9e7244
Zeillons <tgc@live.fr>
2019-03-20 11:02:24 +0100

    sheh

---
+++ github.com/Zeillons/ProjetCi.git/epsi-b3-1819-eval/src/test/java/JUnit5ExampleTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUnit5ExampleTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
